65c82e76831fda24275b8e4885fc6d20e7b305da
Ishara <sachithishara1@gmail.com>
2025-04-27 10:27:31 +0530

    Update category Completed

---
+++ src/app/Features/Category/edit-category/edit-category.component.ts
@@ -0,0 +1,69 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { CategoryService } from '../Services/category.service';
+import { Category } from '../Models/category.model';
+import { UpdateCategoryRequest } from '../Models/update-category-request.model';
+
+@Component({
+  selector: 'app-edit-category',
+  templateUrl: './edit-category.component.html',
+  styleUrls: ['./edit-category.component.css']
+})
+
+export class EditCategoryComponent  implements OnInit ,OnDestroy{
+ id: string | null =null;
+ paramsSubscription ?: Subscription;
+ category?: Category;
+ editCategorySubscription ?: Subscription;
+ 
+  constructor(
+    private route : ActivatedRoute,
+    private categoryService: CategoryService,
+    private router: Router
+  ){
+   
+  }
+
+
+  ngOnInit(): void {
+    this.paramsSubscription =  this.route.paramMap.subscribe({
+      next :(params) =>{
+        this.id =params.get('id');
+
+        if(this.id){
+          //get the category details by id
+          this.categoryService.getCategoryById(this.id)
+          .subscribe({
+            next:(response)=>{
+              this.category =response;
+            }
+          })
+        }
+      }
+    });
+  }
+
+
+  onFormSubmit(): void{
+    const updateCategoryRequest: UpdateCategoryRequest = {
+      name:this.category?.name ?? '',
+      urlHandle: this.category?.urlHandle ?? ''
+    };
+    //pass the object to service
+    if(this.id){
+      this.editCategorySubscription = this.categoryService.updateCategory(this.id,updateCategoryRequest)
+      .subscribe({
+        next:(response)=> {
+            this.router.navigateByUrl('/admin/categories');
+        }
+      });
+    }
+
+  }
+
+  ngOnDestroy(): void {
+    this.paramsSubscription?.unsubscribe();
+    this.editCategorySubscription?.unsubscribe();
+  }
+}
